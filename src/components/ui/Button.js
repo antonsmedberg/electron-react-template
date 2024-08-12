6bf375bbd04ae2d928283f4c40df23eb84b91432
antonsmedberg <83426503+antonsmedberg@users.noreply.github.com>
2024-08-12 22:37:24 +0200

    Update Button.js

--- src/components/ui/Button.js
+++ src/components/ui/Button.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import PropTypes from 'prop-types'
 
 const Button = ({ onClick, children, className }) => (
   <button
@@ -8,12 +7,12 @@ const Button = ({ onClick, children, className }) => (
   >
     {children}
   </button>
-);
+)
 
 Button.propTypes = {
   onClick: PropTypes.func.isRequired,
   children: PropTypes.node.isRequired,
   className: PropTypes.string,
-};
+}
 
-export default Button;
+export default Button
