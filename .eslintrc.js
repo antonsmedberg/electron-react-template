cf01b53a6318cd324dbfabfceb78a532ef2d1846
antonsmedberg <83426503+antonsmedberg@users.noreply.github.com>
2024-08-12 22:36:17 +0200

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,16 +4,13 @@ module.exports = {
     es2021: true,
     node: true,
     jest: true,
-    'jest/globals': true,
   },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
-    'plugin:jsx-a11y/recommended',
     'plugin:import/errors',
     'plugin:import/warnings',
-    'plugin:jest/recommended',
     'plugin:prettier/recommended',
   ],
   parserOptions: {
@@ -23,54 +20,23 @@ module.exports = {
     ecmaVersion: 2021,
     sourceType: 'module',
   },
-  plugins: [
-    'react',
-    'react-hooks',
-    'jsx-a11y',
-    'import',
-    'jest',
-    'prettier',
-  ],
+  plugins: ['react', 'react-hooks', 'import', 'prettier'],
   rules: {
-    'react/prop-types': 'error',
-    'react/jsx-uses-react': 'off',
     'react/react-in-jsx-scope': 'off',
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn',
-    'no-console': ['warn', { allow: ['warn', 'error'] }],
     'import/order': [
       'error',
       {
-        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+        groups: ['builtin', 'external', 'internal'],
         'newlines-between': 'always',
         alphabetize: { order: 'asc', caseInsensitive: true },
       },
     ],
-    'import/no-unresolved': 'error',
-    'jsx-a11y/anchor-is-valid': [
-      'error',
-      {
-        components: ['Link'],
-        specialLink: ['to'],
-      },
-    ],
+    'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
   },
   settings: {
     react: {
       version: 'detect',
     },
-    'import/resolver': {
-      node: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
-      },
-    },
   },
-  overrides: [
-    {
-      files: ['**/*.test.js', '**/*.test.jsx'],
-      env: {
-        jest: true,
-      },
-    },
-  ],
-};
+}
