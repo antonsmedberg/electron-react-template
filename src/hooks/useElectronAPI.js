5eb1b31183065a06c173bd303ac6b00f6f735f30
antonsmedberg <83426503+antonsmedberg@users.noreply.github.com>
2024-08-12 21:17:23 +0200

    Update useElectronAPI.js

--- src/hooks/useElectronAPI.js
+++ src/hooks/useElectronAPI.js
@@ -1,1 +1,15 @@
+import { useEffect, useState } from 'react';
 
+const useElectronAPI = () => {
+  const [electronAPI, setElectronAPI] = useState(null);
+
+  useEffect(() => {
+    if (window.electronAPI) {
+      setElectronAPI(window.electronAPI);
+    }
+  }, []);
+
+  return electronAPI;
+};
+
+export default useElectronAPI;
