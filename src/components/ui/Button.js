e8525d538a036ec5486449f3c318956a2cd63523
antonsmedberg <83426503+antonsmedberg@users.noreply.github.com>
2024-08-12 21:16:53 +0200

    Update Button.js

--- src/components/ui/Button.js
+++ src/components/ui/Button.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Button = ({ onClick, children }) => {
-  return (
-    <button
-      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-      onClick={onClick}
-    >
-      {children}
-    </button>
-  );
-};
+const Button = ({ onClick, children, className }) => (
+  <button
+    className={`px-4 py-2 rounded bg-blue-500 text-white hover:bg-blue-600 ${className}`}
+    onClick={onClick}
+  >
+    {children}
+  </button>
+);
 
 Button.propTypes = {
   onClick: PropTypes.func.isRequired,
   children: PropTypes.node.isRequired,
+  className: PropTypes.string,
 };
 
 export default Button;
