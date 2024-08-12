3eca7431da03e4ce486a842314f5987418c289da
antonsmedberg <83426503+antonsmedberg@users.noreply.github.com>
2024-08-12 21:18:02 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,28 @@
+import React from 'react';
+import Button from './components/ui/Button';
+import useElectronAPI from './hooks/useElectronAPI';
 
+function App() {
+  const electronAPI = useElectronAPI();
+
+  const handleClick = () => {
+    if (electronAPI) {
+      electronAPI.sendMessage('Hello from React!');
+    }
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="text-3xl font-bold underline">
+          Welcome to Electron React
+        </h1>
+        <Button onClick={handleClick}>
+          Send Message to Electron
+        </Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
