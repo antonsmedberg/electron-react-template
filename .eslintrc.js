cb617165d25f46f7ea153bc394daff4acf31053e
antonsmedberg <83426503+antonsmedberg@users.noreply.github.com>
2024-08-13 13:48:53 +0200

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,42 +1,25 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true,
-    jest: true,
-  },
   extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
+    'react-app',
+    'react-app/jest',
     'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
     'plugin:import/errors',
     'plugin:import/warnings',
-    'plugin:prettier/recommended',
+    'plugin:jest/recommended',
+    'plugin:prettier/recommended'
   ],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 2021,
-    sourceType: 'module',
-  },
-  plugins: ['react', 'react-hooks', 'import', 'prettier'],
   rules: {
-    'react/react-in-jsx-scope': 'off',
-    'import/order': [
-      'error',
-      {
-        groups: ['builtin', 'external', 'internal'],
-        'newlines-between': 'always',
-        alphabetize: { order: 'asc', caseInsensitive: true },
-      },
-    ],
-    'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-    'prettier/prettier': ['error', { endOfLine: 'auto' }],
-  },
-  settings: {
-    react: {
-      version: 'detect',
-    },
-  },
-}
+    'prettier/prettier': ['error', {
+      endOfLine: 'auto',
+    }],
+    'import/order': ['error', {
+      groups: ['builtin', 'external', 'internal'],
+      'newlines-between': 'always',
+      alphabetize: {
+        order: 'asc',
+        caseInsensitive: true
+      }
+    }]
+  }
+};
